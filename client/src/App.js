3edcca25442101b5b9f02860dadbda85aefab14b
Cook1619 <cook6348@gmail.com>
2019-09-18 19:26:18 -0500

    Set up mongo atlas

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 //adding to commit
 class App extends Component {
   render() {
-    return <div>I'M READY TO USE THE BACK END APIS! :-)</div>;
+    return <div>Install Atlas, its a mongo db tool were each gonna have to have our own local db until we go to heroku or firebase</div>;
   }
 }
 
